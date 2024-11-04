a21308a0b938e9595981210291dc9c6345f778c7
SundayV <116257793+SundayVHan@users.noreply.github.com>
2024-11-04 16:15:55 +0800

    Update model.go

--- internal/identification/model.go
+++ internal/identification/model.go
@@ -14,24 +14,6 @@ var (
 	filePath        string
 )
 
-func init() {
-	// 设置 viper 读取配置文件的名称和格式
-	viper.SetConfigName("config") // 指定配置文件名称（不含扩展名）
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath("./config") // 假设 config.yaml 在项目根目录
-
-	// 读取配置文件
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file: %v", err)
-	}
-
-	// 解析配置项
-	accessKeyId = viper.GetString("aliyun.accessKeyId")
-	accessKeySecret = viper.GetString("aliyun.accessKeySecret")
-	endpoint = viper.GetString("aliyun.endpoint")
-	bucketName = viper.GetString("aliyun.bucketName")
-	filePath = viper.GetString("aliyun.filePath")
-}
 
 type QARequest struct {
 	Image string `json:"image" binding:"required"`
